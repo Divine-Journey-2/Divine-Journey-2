6ca40a79c6f6bca74a08cdf1bb54a3ec6111feee
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2023-08-20 07:02:46 -0700

    add build (#1035)

---
+++ serverfiles/launch.bat
@@ -0,0 +1,45 @@
+:: Server Launch Script
+::
+:: Thrown together by Neeve in under five minutes, Public Domain
+:: https://github.com/Neeve01
+::
+:: Added java version check by t0suj4, Public Domain
+:: https://github.com/t0su4
+::
+:: Adapted from Nomifactory by WaitingIdly, adding configurable java version and instructions
+:: https://github.com/Nomifactory/Nomifactory/blob/f05d2f552ca8441c3f26fff76e16392c74f12337/launchscripts/launch.bat
+::
+
+:: DO NOT EDIT UNLESS YOU KNOW WHAT YOU'RE DOING
+@ECHO OFF
+SET FORGE_JAR=forge-1.12.2-14.23.5.2860.jar
+
+:: these you can edit
+:: this must point to Java 1.8, edit it to point directly to a valid java version if it does not.
+:: if you get a `launcher.java.lang.reflect.InvocationTargetException`, change this
+SET JAVA_VERSION=java
+:: a good rule of thumb is 2gb+1gb per concurrent player.
+SET MIN_RAM=4096M
+SET MAX_RAM=4096M
+SET JAVA_PARAMETERS=
+
+:: DO NOT EDIT ANYTHING PAST THIS LINE
+SET LAUNCHPARAMS=-server -Xms%MIN_RAM% -Xmx%MAX_RAM% %JAVA_PARAMETERS% -jar %FORGE_JAR% nogui
+
+echo Checking java version...
+echo.
+%JAVA_VERSION% -version
+echo.
+echo The expected java version is 1.8. Not higher, not lower.
+echo.
+
+echo Launching the server...
+echo.
+echo ^> %JAVA_VERSION% %LAUNCHPARAMS%
+echo.
+%JAVA_VERSION% %LAUNCHPARAMS%
+
+echo.
+echo ^> The server has stopped. If it's a crash, please read the output above.
+echo.
+pause
