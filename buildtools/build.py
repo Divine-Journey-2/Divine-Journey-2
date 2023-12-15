1d04e74c64ff98f365a15d8d7d42d1d377607abd
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2023-12-14 20:57:16 -0800

    remove commented out code

--- buildtools/build.py
+++ buildtools/build.py
@@ -331,14 +331,6 @@ def copyServer(manifest):
     for dir in serverCopyDirs:
         try:
             shutil.copytree(basePath + "/overrides/" + dir, server + "/" + dir)
-
-    # Copy all lang files
-    # Currently does nothing, as no resources are loaded serverside, which is
-    # incorrect behavior as multiple locations need a lang file loaded serverside
-    # for root, _, files in os.walk(basePath + "/overrides/"):
-    #     for file in files:
-    #         if (os.path.normcase(file)[-4:] == '.lang'):
-    #             shutil.copy(os.path.join(root, root.split(basePath + "/overrides")[-1], file), os.path.join(server + "/", file))
         except:
             pass
 
