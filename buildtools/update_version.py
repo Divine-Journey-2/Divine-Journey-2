9a1d7227c4c3efc1fc85ca0c956dd915b83ac19a
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-04-21 18:52:45 -0700

    simplify update_version

--- buildtools/update_version.py
+++ buildtools/update_version.py
@@ -22,43 +22,10 @@ def parse_args():
                         type=str,
                         required=True,
                         help="the version being updated to")
-    parser.add_argument("--old", "-O",
-                        type=str,
-                        required=False,
-                        help="version being updated from, defaults to the latest git tag")
     return parser.parse_args()
 
 
 basePath = path.normpath(path.abspath(f"{__file__}/../../"))
-
-filesToUpdateVersion = [
-    "manifest.json",
-    "overrides/config/CustomMainMenu/mainmenu.json",
-    "overrides/config/mputils/addons/mpbasic/mpbasic.cfg"
-]
-
-
-def oldVersion() -> str:
-    """Get the current tag"""
-    try:
-        return run(["git", "describe", "--abbrev=0", "--tags"], capture_output=True, cwd=basePath).stdout.strip().decode("utf-8")
-    except:
-        print("could not determine git sha, skipping")
-    return ""
-
-
-def updateVersion(old: str, version: str):
-    """Replaces the old version number with the new version number in all targeted files"""
-    for file in filesToUpdateVersion:
-        f = open(file, "r")
-        filedata = f.read()
-        f.close()
-
-        newdata = filedata.replace(old, version)
-
-        with open(file, 'w') as new:
-            new.write(newdata)
-
 
 def convertChangelog(version: str):
     """Converts and filters the existing changelog in the LATEST.md file to a file named after the version"""
@@ -111,16 +78,6 @@ def convertChangelog(version: str):
 def process(args):
     """Converts the pack version in various files and converts the changelog file"""
 
-    old_version = args.old if args.old else oldVersion()
-
-    if old_version == "":
-        sys.exit("old version was not found, exiting")
-
-    if args.version == old_version:
-        print("old version was the same as new version, skipping")
-        return
-
-    updateVersion(old_version, args.version)
     convertChangelog(args.version)
     print("updated version-related files")
 
