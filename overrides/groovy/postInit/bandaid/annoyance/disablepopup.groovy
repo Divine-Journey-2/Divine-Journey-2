c262010c45ed4e9f904fbd77019bcbb7a0b6a409
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2026-01-17 05:26:36 -0800

    disable iwc warning

---
+++ overrides/groovy/postInit/bandaid/annoyance/disablepopup.groovy
@@ -0,0 +1,8 @@
+// side: client
+
+
+// GroovyScript shows a warning screen if InWorldCrafting is installed,
+// telling the user to use GroovyScript's version instead.
+// I don't want to migrate at this point, so just claim it was opened
+// already so it doesn't appear.
+com.cleanroommc.groovyscript.compat.WarningScreen.wasOpened = true
