0a20e2c35119941b9c2eb30fa996ff6b36432aa4
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2026-03-11 19:07:15 -0700

    dont register fixers on dedicated server

--- overrides/groovy/classes/fixes/Fixer.groovy
+++ overrides/groovy/classes/fixes/Fixer.groovy
@@ -15,6 +15,7 @@ class Fixer {
     static {
         def fmlFixer = FMLCommonHandler.instance().getDataFixer()
         def fixer = fmlFixer.init(getPackId(), LATEST)
+        if (isDedicatedServer()) return // datafixers seem to break on servers - disable them until the cause can be resolved
         fixer.registerFix(FixTypes.ITEM_INSTANCE, new ActuallyBaubles())
         fixer.registerFix(FixTypes.ITEM_INSTANCE, new MysticalAgricultureTieredCrystals())
         fixer.registerFix(FixTypes.ITEM_INSTANCE, new OmniwandConversion())
