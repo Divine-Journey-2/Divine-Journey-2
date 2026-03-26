3d87599334ed22a4ccde4ee93895dd3c4d5b21d6
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2026-03-26 11:32:18 -0700

    gas tank size increase

--- overrides/groovy/preInit/power_rebalance.groovy
+++ overrides/groovy/preInit/power_rebalance.groovy
@@ -7,3 +7,11 @@ PowerSystem.RedstoneFlux.maxCapacity = 10_000_000_000L
 
 // turbine doesn't work because it is a static final field - will have to use mixins to change to 50 billion
 
+
+import mekanism.common.tile.prefab.TileEntityAdvancedElectricMachine
+
+// increase the base MAX_GAS that can go into a gas tank for mekanism from 210 to 2100
+// primarily for the Osmium Compressor, as Osmium Blocks give 1800 gas and so can't ever be converted
+TileEntityAdvancedElectricMachine.metaClass.makeMutable('MAX_GAS')
+TileEntityAdvancedElectricMachine.MAX_GAS = 2100
+
