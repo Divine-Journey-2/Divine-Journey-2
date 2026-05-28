3767d8b5e0c73c72ce276f9bf2e1b5bd16103fa4
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2026-05-28 07:12:17 -0700

    Bewitchment Altar Upgrades (#1333)

---
+++ overrides/groovy/classes/content/jei/MultiIngredientDrawable.groovy
@@ -0,0 +1,32 @@
+package classes.content.jei
+
+import mezz.jei.api.gui.IDrawable
+import mezz.jei.api.gui.IDrawableAnimated
+import mezz.jei.api.gui.ITickTimer
+import net.minecraft.client.Minecraft
+
+/// animated drawable that cycles between drawing the listed ingredients
+class MultiIngredientDrawable implements IDrawableAnimated {
+
+    final ITickTimer timer
+    final List<IDrawable> ingredients
+
+    MultiIngredientDrawable(ITickTimer timer, List<IDrawable> ingredients) {
+        this.timer = timer
+        this.ingredients = ingredients
+    }
+
+	int getWidth() {
+        16
+    }
+
+	int getHeight() {
+        16
+    }
+
+	void draw(Minecraft minecraft, int xOffset, int yOffset) {
+        if (ingredients.isEmpty()) return
+        int time = timer.getValue() % ingredients.size()
+        ingredients[time].draw(minecraft, xOffset, yOffset)
+    }
+}
